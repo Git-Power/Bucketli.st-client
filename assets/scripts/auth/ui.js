d2577309f33557989d6c8f4e9049597392d6a855
JeanaDeLaire <jeanadelaire@gmail.com>
2018-12-20 20:52:34 -0500

    searchbar clear and minor style fix

--- assets/scripts/auth/ui.js
+++ assets/scripts/auth/ui.js
@@ -1,54 +1,53 @@
 const store = require('../store.js')
 const bucketEvents = require('../buckets/events.js')
 
-const signUpSuccess = function(data) {
-    $('#message-up').text('Signed up successfully')
-    $('#message-up').removeClass()
-    $('#message-up').addClass('success')
-    $('#sign-up-form')[0].reset()
+const signUpSuccess = function (data) {
+  $('#message-up').text('Signed up successfully')
+  $('#message-up').removeClass()
+  $('#message-up').addClass('success')
+  $('#sign-up-form')[0].reset()
 }
 
-const signUpFailure = function (error) {
-    $('#message-up').text('There has been an error, please try again.')
-    $('#message-up').removeClass()
-    $('#message-up').addClass('failure')
+const signUpFailure = function () {
+  $('#message-up').text('There has been an error, please try again.')
+  $('#message-up').removeClass()
+  $('#message-up').addClass('failure')
 }
 
 const signInSuccess = function (data) {
-    store.user = data.user
-    $('#sign-in').modal('toggle')
-    $('#homescreen-jpg').css('filter', 'blur(0px)')
-    bucketEvents.onGetAllBuckets()
+  store.user = data.user
+  $('#sign-in').modal('toggle')
+  $('#homescreen-jpg').css('filter', 'blur(0px)')
+  bucketEvents.onGetAllBuckets()
 }
 
-
-const signInFailure = function (error) {
-    $('#message-up').text('Wrong E-mail or Password')
-    $('#message-up').removeClass()
-    $('#message-up').addClass('failure')
+const signInFailure = function () {
+  $('#message-up').text('Wrong E-mail or Password')
+  $('#message-up').removeClass()
+  $('#message-up').addClass('failure')
 }
 
 const signOutSuccess = function (data) {
-    $('#sign-up').modal()
-    $('#home-screen').show()
-    $('#homescreen-jpg').css('filter', 'blur(7px)')
-    $('#sign-in-form')[0].reset()
+  $('#sign-up').modal()
+  $('#home-screen').show()
+  $('#homescreen-jpg').css('filter', 'blur(7px)')
+  $('#sign-in-form')[0].reset()
 }
 
 const changePasswordSuccess = function (data) {
-    $('#message-password').text('Changed password successfully')
+  $('#message-password').text('Changed password successfully')
 }
 
-const changePasswordFailure = function (error) {
-    $('#message-password').text('Password is incorrect')
+const changePasswordFailure = function () {
+  $('#message-password').text('Password is incorrect')
 }
 
 module.exports = {
-    signUpSuccess,
-    signUpFailure,
-    signInSuccess,
-    signInFailure,
-    signOutSuccess,
-    changePasswordSuccess,
-    changePasswordFailure
+  signUpSuccess,
+  signUpFailure,
+  signInSuccess,
+  signInFailure,
+  signOutSuccess,
+  changePasswordSuccess,
+  changePasswordFailure
 }
--- assets/scripts/buckets/api.js
+++ assets/scripts/buckets/api.js
@@ -1,4 +1,4 @@
-const config = require ('../config.js')
+const config = require('../config.js')
 const store = require('../store.js')
 
 const uploadBucket = function (form) {
